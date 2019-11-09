5db6aef63480b9a01f295a50ec9e6afa29d8d940
Cupiron <romansaulenko@gmail.com>
2019-11-09 15:24:11 +0300

    First commit

---
+++ database/migrations/2019_11_09_121602_create_nomenclatures_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNomenclaturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nomenclatures', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title', 100);
+            $table->double('price_retail');
+            $table->double('price_procurement');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nomenclatures');
+    }
+}
